71d389d3037b68c9b949160f949bc874a37ec626
Andrei Cataraga <andreicataraga18@gmail.com>
2024-01-30 22:01:34 +0100

    New Changes in EmployeeDAOImpl

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/dao/EmployeeDAOImpl.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/dao/EmployeeDAOImpl.java
@@ -1,7 +1,6 @@
 package com.zaurtregulov.spring.mvc_hibernate_aop.entity.dao;
 
 import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public class EmployeeDAOImpl implements EmployeeDAO {
+public abstract class EmployeeDAOImpl implements EmployeeDAO {
 
     @Autowired
     private SessionFactory sessionFactory;
@@ -34,15 +33,5 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         Session session = sessionFactory.getCurrentSession();
         Employee employee = session.get(Employee.class, id);
         return employee;
-    }
-
-    @Override
-    public void deleteEmployee(int id) {
-        Session session = sessionFactory.getCurrentSession();
-//        Query<Employee> query=session.createQuery("delete from Employee" + "where id =:employeeId");
-        Query<Employee> query = session.createQuery("delete from Employee where id = :employeeId");
-
-        query.setParameter("employeeId", id);
-        query.executeUpdate();
     }
 }
