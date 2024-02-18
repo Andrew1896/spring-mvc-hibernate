41e24e71953402e1e15a662504d7a4c1f807b85f
Andrei Cataraga <andreicataraga18@gmail.com>
2024-02-18 23:42:10 +0100

    Spring MVC+Hinernate+AOP

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -1,11 +1,10 @@
 package com.zaurtregulov.spring.mvc_hibernate_aop.controller;
 
 import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
-import com.zaurtregulov.spring.mvc_hibernate_aop.service.EmployeeService;
+import com.zaurtregulov.spring.mvc_hibernate_aop.entity.dao.EmployeeDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,26 +13,26 @@ import java.util.List;
 public class MyController {
 
     @Autowired
-//    private EmployeeDAO employeeDAO;
-    private EmployeeService employeeService;
+    private EmployeeDAO employeeDAO;
+//    private EmployeeService employeeService;
 
     @RequestMapping("/")
     public String showAllEmployees(Model model) {
-        List<Employee> allEmployees = employeeService.getAllEmployees();
+        List<Employee> allEmployees = employeeDAO.getAllEmployees();
         model.addAttribute("allEmps", allEmployees);
 
         return "all-employees";
     }
-    @RequestMapping("/addNewEmployee")
-        public String addNewEmployee(Model model) {
-        Employee employee = new Employee();
-        model.addAttribute("employee", employee);
-        return "employee-info";
-
-    }
-    @RequestMapping("saveEmployee")
-    public String saveEmployee (@ModelAttribute("employee") Employee employee) {
-        employeeService.saveEmployee(employee);
-        return "redirect:/";
-    }
+//    @RequestMapping("/addNewEmployee")
+//        public String addNewEmployee(Model model) {
+//        Employee employee = new Employee();
+//        model.addAttribute("employee", employee);
+//        return "employee-info";
+//
+//    }
+//    @RequestMapping("saveEmployee")
+//    public String saveEmployee (@ModelAttribute("employee") Employee employee) {
+//        employeeService.saveEmployee(employee);
+//        return "redirect:/";
+//    }
 }
--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/dao/EmployeeDAOImpl.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/dao/EmployeeDAOImpl.java
@@ -5,8 +5,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 @Repository
 public abstract class EmployeeDAOImpl implements EmployeeDAO {
 
@@ -14,7 +16,7 @@ public abstract class EmployeeDAOImpl implements EmployeeDAO {
     private SessionFactory sessionFactory;
 
     @Override
-//    @Transactional
+    @Transactional
     public List<Employee> getAllEmployees() {
         Session session = sessionFactory.getCurrentSession();
         List<Employee> allEmployees = session.createQuery("from Employee",
