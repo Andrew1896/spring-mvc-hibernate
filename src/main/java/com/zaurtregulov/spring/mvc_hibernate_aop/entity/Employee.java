3684e00d8ff3e483998d1b4a7d6565b511502021
Andrei Cataraga <andreicataraga18@gmail.com>
2024-02-20 23:34:26 +0100

    Update Employee

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/Employee.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/Employee.java
@@ -33,16 +33,16 @@ public class Employee {
         this.salary = salary;
     }
 
-//    @Override
-//    public String toString() {
-//        return "Employee{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", surname='" + surname + '\'' +
-//                ", department='" + department + '\'' +
-//                ", salary=" + salary +
-//                '}';
-//    }
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 
     public int getId() {
         return id;
--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/dao/EmployeeDAOImpl.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/dao/EmployeeDAOImpl.java
@@ -1,12 +1,12 @@
 package com.zaurtregulov.spring.mvc_hibernate_aop.entity.dao;
 
 import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Repository
@@ -19,11 +19,11 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Transactional
     public List<Employee> getAllEmployees() {
         Session session = sessionFactory.getCurrentSession();
-//        List<Employee> allEmployees = session.createQuery("from Employee",
-//                Employee.class).getResultList();
+        List<Employee> allEmployees = session.createQuery("from Employee",
+                Employee.class).getResultList();
 
-        Query<Employee> query = session.createQuery("from Employee", Employee.class );
-        List<Employee> allEmployees = query.getResultList();
+//        Query<Employee> query = session.createQuery("from Employee", Employee.class );
+//        List<Employee> allEmployees = query.getResultList();
         return allEmployees;
     }
 
